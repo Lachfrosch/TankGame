9796ce7e64e940a6942a7f6fe0196411cc69cd5e
Jo2We <johannes.welsch.eins@gmx.de>
2023-04-27 16:27:07 +0200

    implemented recoil and dampening for landing. Fixed jumping

--- Assets/NetcodeForGameObjects/Scripts/ProjectileTurret.cs
+++ Assets/NetcodeForGameObjects/Scripts/ProjectileTurret.cs
@@ -35,9 +35,15 @@ public class ProjectileTurret : NetworkBehaviour
 
     private StarterAssetsInputs _input;
 
+    // recoil
+    private float _recoil = 10000.0f;
+
+    // rigidbody
+    Rigidbody rb;
 
     private void Awake()
     {
+        rb = GetComponent<Rigidbody>();
         //Fill Magazine
         bulletsLeft = magazineSize;
         readyToShoot = true;
@@ -113,6 +119,10 @@ public class ProjectileTurret : NetworkBehaviour
         }
 
         SpawnBulletServerRpc(targetPoint, attackPoint.position, Quaternion.identity);
+
+        // recoil
+        Vector3 force = attackPoint.TransformDirection(Vector3.back);
+        rb.AddForceAtPosition(force * _recoil, attackPoint.position, ForceMode.Impulse);
 
         bulletsLeft--;
         bulletsShot++;
